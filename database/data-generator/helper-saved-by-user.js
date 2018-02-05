1eddf8a9f32f3bd2e2351924000b3c85b8ff398c
Angela Chang <ajkchang@gmail.com>
2018-02-04 22:50:10 -0800

    redid tables and started importing to dbase

--- database/data-generator/helper-saved-by-user.js
+++ database/data-generator/helper-saved-by-user.js
@@ -2,10 +2,8 @@ const faker = require('faker');
 const fs = require('fs');
 
 let lies = fs.createWriteStream('liesbysaved.csv');
-for (var i = 0; i < 3000000; i++) {
-  let userid = faker.random.number();
-  let list = ['BURN-E',
-  'Shanghai Knights',
+for (var i = 1; i <= 5000000; i++) {
+  let list = [
   'Ewoks: The Battle for Endor',
   'Hatchet III',
   'Just Like Me (Igualita a Mi)',
@@ -15,52 +13,22 @@ for (var i = 0; i < 3000000; i++) {
   'Citizen Cohn',
   'Rocks in my Pockets',
   'The Widow From Chicago',
-  'Billy Jack Goes to Washington'];
-  let title = faker.random.arrayElement([
+  'Billy Jack Goes to Washington',
   'BURN-E',
   'Shanghai Knights',
-  'Ewoks: The Battle for Endor',
-  'Hatchet III',
-  'Just Like Me (Igualita a Mi)',
-  'Three on a Match',
-  'Sione\'s Wedding (Samoan Wedding)',
-  'Messenger: The Story of Joan of Arc, The',
-  'Citizen Cohn',
-  'Rocks in my Pockets',
-  'The Widow From Chicago',
-  'Billy Jack Goes to Washington'
-  ]);
-  let videoid = list.indexOf(title);
-  let title2 = faker.random.arrayElement([
-    'BURN-E',
-    'Shanghai Knights',
-    'Ewoks: The Battle for Endor',
-    'Hatchet III',
-    'Just Like Me (Igualita a Mi)',
-    'Three on a Match',
-    'Sione\'s Wedding (Samoan Wedding)',
-    'Messenger: The Story of Joan of Arc, The',
-    'Citizen Cohn',
-    'Rocks in my Pockets',
-    'The Widow From Chicago',
-    'Billy Jack Goes to Washington'
-  ]);
-  let videoid2 = list.indexOf(title2);
-  let title3 = faker.random.arrayElement([
-    'BURN-E',
-    'Shanghai Knights',
-    'Ewoks: The Battle for Endor',
-    'Hatchet III',
-    'Just Like Me (Igualita a Mi)',
-    'Three on a Match',
-    'Sione\'s Wedding (Samoan Wedding)',
-    'Messenger: The Story of Joan of Arc, The',
-    'Citizen Cohn',
-    'Rocks in my Pockets',
-    'The Widow From Chicago',
-    'Billy Jack Goes to Washington'
-  ]);
-  let videoid3 = list.indexOf(title3);
-  lies.write(`${userid}; ${title}; ${title2}; ${title3} \n`);
+  'Franz Kafka\'s a Country Doctor',
+  'In Which We Serve',
+  'Quigley Down Under',
+  'Born to Be Wild',
+  'Firelight',
+  'Miami Rhapsody',
+  'Faithless',
+  'Hansel & Gretel Get Baked'
+  ];
+  let title = faker.random.arrayElement(list);
+  let title2 = faker.random.arrayElement(list);
+  let title3 = faker.random.arrayElement(list);
+
+  lies.write(`${i}; ${title}; ${title2}; ${title3} \n`);
 }
 lies.end();
--- database/data-generator/helper-videos-by-region2.js
+++ database/data-generator/helper-videos-by-region2.js
@@ -1,19 +1,19 @@
 const faker = require('faker');
 const fs = require('fs');
 
-let lies = fs.createWriteStream('liesbyregion.csv');
-for (var i = 1; i <= 5000000; i++) {
+let lies = fs.createWriteStream('liesbyregion2.csv');
+for (var i = 5000001; i <= 10000000; i++) {
   let titlelist = [
-    'Ewoks: The Battle for Endor',
-    'Hatchet III',
-    'Just Like Me (Igualita a Mi)',
-    'Three on a Match',
-    'Sione\'s Wedding (Samoan Wedding)',
-    'Messenger: The Story of Joan of Arc, The',
-    'Citizen Cohn',
-    'Rocks in my Pockets',
-    'The Widow From Chicago',
-    'Billy Jack Goes to Washington'
+    'BURN-E',
+    'Shanghai Knights',
+    'Franz Kafka\'s a Country Doctor',
+    'In Which We Serve',
+    'Quigley Down Under',
+    'Born to Be Wild',
+    'Firelight',
+    'Miami Rhapsody',
+    'Faithless',
+    'Hansel & Gretel Get Baked'
   ];
   let genrelist = [
     'action',
