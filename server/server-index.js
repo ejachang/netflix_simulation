cac03626687a688eefc112e6d7560b0a122c165a
Angela Chang <ajkchang@gmail.com>
2018-02-05 11:24:46 -0800

    working on dbase

--- server/server-index.js
+++ server/server-index.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(ctx => {
-  ctx.body = 'You can put in an effort, but if you\'re stupid, you\'re stupid';
+  ctx.body = 'You can always put in an effort, but if you\'re stupid, you\'re stupid';
 });
 
 app.listen(8886);
